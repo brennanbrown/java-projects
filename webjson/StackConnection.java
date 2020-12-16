0a5c7fd764e245f5f8a1d909f25e3c64ce8a5f9a
Brennan Brown <brennan.brown@edu.sait.ca>
2020-12-15 18:57:55 -0700

    Add initial API functionality

--- webjson/StackConnection.java
+++ webjson/StackConnection.java
@@ -9,4 +9,35 @@ public class StackConnection {
 	private static final String PARAM_TAG = "&tagged=";
 	private static final String PARAM_FILTER = "&filter=";
 	private static final String PARAM_SEARCH_TERM = "&intitle=";
+	
+	private String site = "stackoverflow";
+	private int pageSize = 5;
+	private SortOrder sortOrder = SortOrder.ASCENDING;
+	private SortBy sortBy = SortBy.RELEVANCE;
+	private String tag = "java";
+	private String searchTerm = "";
+	
+	public enum SortOrder {
+		ASCENDING   ("asc"), 
+		DESCENDING  ("desc");
+		
+		private final String key;
+		SortOrder(String key) { this.key = key; }
+		
+		public String toString() { return key; }
+	}
+	
+	
+	public enum SortBy {
+		RELEVANCE	("relevance"), 
+		VOTES		("votes"), 
+		CREATION	("creation"), 
+		ACTIVITY	("activity");
+		
+		private final String key;
+		SortBy(String key) { this.key = key; }
+		
+		public String toString() { return key; }
+	}
+	
 }
